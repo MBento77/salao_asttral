b9e6e2c900a4a76bf25adb24bb2f70b1e9301469
Matheus Bento <matheussb220104@gmail.com>
2024-05-18 21:50:53 -0300

    atualizando audio

---
+++ js/audio.js
@@ -0,0 +1,20 @@
+     document.addEventListener('DOMContentLoaded', (event) => {
+        const audio = document.getElementById('background-audio');
+
+        // Adiciona um evento de click ao documento para iniciar o áudio após interação do usuário
+        const enableAudio = () => {
+            audio.play().then(() => {
+                // Autoplay iniciado
+                console.log('Autoplay iniciado após interação do usuário.');
+            }).catch(error => {
+                // Erro ao tentar iniciar o autoplay
+                console.log('Erro ao iniciar o autoplay:', error);
+            });
+
+            // Remove o evento de click após a primeira interação para evitar múltiplas chamadas
+            document.removeEventListener('click', enableAudio);
+        };
+
+        // Adiciona o listener para iniciar o áudio após a interação do usuário
+        document.addEventListener('click', enableAudio);
+    });
